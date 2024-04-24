c98bd32fe9b0e2593e866c228bd4aa9d1d15a7c1
DandinPower <tomhot246@gmail.com>
2024-04-24 20:32:26 +0800

    Modify the dataset to better fit the SFT scenraio

--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,4 @@
+from transformers import AutoTokenizer
 import random
 import pandas as pd
 import json
@@ -15,6 +16,7 @@ class Data:
     id: str
     instruction: str
     output: str
+    text: str
 
 
 def create_dir(path):
@@ -26,14 +28,31 @@ def preprocess_text(text) -> str:
     text = str(text)
     text = text.replace('\n', '')
     text = text.replace('\t', '')
+    text = text.replace('x0000', '')
+    text = text.replace('_____________________x0006_____䋊__䎴ꀀ䒤__䍺____Ṱ෰_____x0005________________________________x0003_____䋊耀䎃__䏞__䈴____Ṱ෰_____x0001_______衐ෟ_____________________x001B_____________䒴耀䐡____Ṱ෰_____x0005_______衐ෟ_____________________x0003_____䆀耀䐚__䍓__䇠____Ṱ෰_____x0001_______________________________________________________________징', '')
     return text
 
 
 def write_tsv(data: list[Data], path: str):
     with open(path, 'w', encoding='utf-8') as f:
-        f.write('id\tinstruction\toutput\n')
+        f.write('id\tinstruction\toutput\ttext\n')
         for d in data:
-            f.write(f'{d.id}\t{d.instruction}\t{d.output}\n')
+            f.write(f'{d.id}\t{d.instruction}\t{d.output}\t{d.text}\n')
+
+
+def use_tokenizer_chat_template(tokenizer, instruction: str, output: str) -> str:
+    # test
+    if output == "-1":
+        chat = [
+            {"role": "user", "content": instruction}
+        ]
+    # train or valid
+    else:
+        chat = [
+            {"role": "user", "content": instruction},
+            {"role": "assistant", "content": output},
+        ]
+    return tokenizer.apply_chat_template(chat, tokenize=False)
 
 
 def main(args):
@@ -44,6 +63,8 @@ def main(args):
     output_dir = args.output_dir
     train_valid_split = args.train_valid_split
     create_dir(output_dir)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
 
     # Train dataset
     df = pd.read_excel(train_path)
@@ -56,10 +77,13 @@ def main(args):
         item3 = preprocess_text(df['選項3'][i])
         item4 = preprocess_text(df['選項4'][i])
         answer = preprocess_text(df['正確答案'][i])
+        instruction = f'{INSTRUCTION_START};說明:{document};問題:{question}1:{item1},2:{item2},3:{item3},4:{item4}'
         original_train_data.append(Data(
             id=str(i),
-            instruction=f'{INSTRUCTION_START};說明:{document};問題:{question}1:{item1},2:{item2},3:{item3},4:{item4}',
-            output=answer
+            instruction=instruction,
+            output=answer,
+            text=preprocess_text(use_tokenizer_chat_template(
+                tokenizer, instruction, answer))
         ))
 
     random.shuffle(original_train_data)
@@ -80,10 +104,13 @@ def main(args):
         item2 = preprocess_text(df['選項2'][i])
         item3 = preprocess_text(df['選項3'][i])
         item4 = preprocess_text(df['選項4'][i])
+        instruction = f'{INSTRUCTION_START};說明:{document};問題:{question}1:{item1},2:{item2},3:{item3},4:{item4}'
         test_data.append(Data(
-            id=str(df['題號'][i]),
-            instruction=f'{INSTRUCTION_START};說明:{document};問題:{question}1:{item1},2:{item2},3:{item3},4:{item4}',
-            output="-1"
+            id=str(i),
+            instruction=instruction,
+            output="-1",
+            text=preprocess_text(use_tokenizer_chat_template(
+                tokenizer, instruction, "-1"))
         ))
 
     write_tsv(test_data,
@@ -97,6 +124,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--tokenizer_name', type=str, required=True)
     parser.add_argument(
         '--train_path',
         default=None,
--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@ TEST_PATH=datasets/AI1000.xlsx
 OUTPUT_DIR=output
 TRAIN_VALID_SPLIT=0.8
 SEED=42
-HF_DATASET_NAME=ZH-Reading-Comprehension
+HF_DATASET_NAME=ZH-Reading-Comprehension-Llama3-Instruct
+# TOKENIZER_NAME=MediaTek-Research/Breeze-7B-Instruct-v1_0
+TOKENIZER_NAME=meta-llama/Meta-Llama-3-8B-Instruct
+# TOKENIZER_NAME=taide/TAIDE-LX-7B-Chat
+# TOKENIZER_NAME=google/gemma-1.1-7b-it
 
 rm -rf $OUTPUT_DIR
 
 python create_dataset.py \
+    --tokenizer_name $TOKENIZER_NAME \
     --train_path $TRAIN_PATH \
     --test_path $TEST_PATH \
     --output_dir $OUTPUT_DIR \
